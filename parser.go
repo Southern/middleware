ee5ff703f137ffac5ee4a539970fed67a3adf6b3
Colton Baker <github@netrefuge.net>
2014-01-10 18:34:47 -0500

    Move from req.Map to req.Map["json"] to prevent clashing

--- parser.go
+++ parser.go
@@ -6,16 +6,23 @@ import (
   "io/ioutil"
 )
 
+type JSON struct {
+  Map map[string]interface{}
+}
+
 func BodyParser(req *stackr.Request, res *stackr.Response, next func()) {
   defer next()
   contentType := req.Header.Get("content-type")
   if contentType[0:16] == "application/json" {
-    req.Map = make(map[string]interface{})
+    if req.Map == nil {
+      req.Map = make(map[string]interface{})
+    }
+    req.Map["json"] = &JSON{ make(map[string]interface{}) }
     body, err := ioutil.ReadAll(req.Request.Body)
     if err != nil {
       return
     }
-    err = json.Unmarshal(body, &req.Map)
+    err = json.Unmarshal(body, req.Map["json"].(JSON).Map)
     if err != nil {
       return
     }
