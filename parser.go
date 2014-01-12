49fce0f98c9cb91521a15434e71a54582375858d
Colton Baker <github@netrefuge.net>
2014-01-12 18:38:27 -0500

    Switch everything over to a simple map[string]interface{}
    The JSON struct isn't working out like I wanted it to.

--- parser.go
+++ parser.go
@@ -6,25 +6,23 @@ import (
   "io/ioutil"
 )
 
-type JSON struct {
-  Map map[string]interface{}
-}
-
 func BodyParser(req *stackr.Request, res *stackr.Response, next func()) {
+  var j map[string]interface{}
   defer next()
   contentType := req.Header.Get("content-type")
   if contentType[0:16] == "application/json" {
     if req.Map == nil {
       req.Map = make(map[string]interface{})
     }
-    req.Map["json"] = &JSON{ make(map[string]interface{}) }
+    req.Map["json"] = make(map[string]interface{})
     body, err := ioutil.ReadAll(req.Request.Body)
     if err != nil {
       return
     }
-    err = json.Unmarshal(body, req.Map["json"].(JSON).Map)
+    err = json.Unmarshal(body, &j)
     if err != nil {
       return
     }
+    req.Map["json"] = j
   }
 }
